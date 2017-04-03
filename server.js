f7771c6fb1d2234a214b7ee10fce1496b954f368
alex <alex@admins-Air.Dlink>
2017-04-03 21:39:05 +0300

    some html

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ connections = [];
 server.listen(process.env.PORT || 3000);
 
 console.log('Server running');
+
 app.get('/', function(req, res) {
    res.sendFile(__dirname + '/index.html');
+});
+
+
+io.sockets.on('connection', function(socket) {
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length );
+
+  //Disconnect
+  connections.splice(connections.indexOf(socket), 1);
+  console.log('Disconnected: %s sockets disconnected', connections.length); 
 });
